b324ed62d06dabcf557b71454e2e7447858853c0
bkgoh27 <bengkoon.goh27@gmail.com>
2022-04-03 14:52:57 +0800

    switch case with days

--- dayOfTheWeek.go
+++ dayOfTheWeek.go
@@ -1,5 +1,25 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
 func main() {
-	//Insert code here
+	// switch ch := "b"; ch {
+	// case "a":
+	// 	fmt.Println("a")
+	// case "b", "c":
+	// 	fmt.Println("b or c")
+	// default:
+	// 	fmt.Println("No matching character")
+	// }
+	switch day := time.Now().Weekday(); day {
+	case time.Monday, time.Wednesday, time.Friday, time.Sunday:
+		fmt.Println(day, "- odd day")
+	case time.Tuesday, time.Thursday, time.Saturday:
+		fmt.Println(day, "- even day")
+	default:
+		fmt.Println("what is this day? ", day)
+	}
 }
